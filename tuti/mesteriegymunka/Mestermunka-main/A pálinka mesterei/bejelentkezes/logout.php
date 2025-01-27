6a8b7ad6632b6a986f6bfb490ef99718410bb713
rozlev <rozlev404@hengersor.hu>
2025-01-27 11:49:21 +0100

    jatekfejlesztes

--- tuti/mesteriegymunka/Mestermunka-main/A pálinka mesterei/bejelentkezes/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // A munkamenet indítása
-session_unset(); // Az összes munkameneti változó törlése
-session_destroy(); // A munkamenet lezárása
-
-header("Location: bejelentkezes/bejelentkezes.html"); // Átirányítás a bejelentkezési oldalra
-exit();
-?>
