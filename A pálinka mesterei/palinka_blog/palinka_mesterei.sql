f0e8e01894d55f81f5a901d5778104e5e77adaee
Atmff <nemben431@hengersor.hu>
2025-03-11 08:39:53 +0100

    alap adminnal adatbazis

--- A pálinka mesterei/palinka_blog/palinka_mesterei.sql
+++ A pálinka mesterei/palinka_blog/palinka_mesterei.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Már 11. 08:39
+-- Létrehozás ideje: 2025. Már 11. 08:38
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
